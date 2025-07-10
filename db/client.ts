013f4c6f3629934d07873de45452f9fdf247c440
3b7577 <3b7577@gmail.com>
2025-07-10 12:56:36 +0300

    Fix db schema and connection

--- db/client.ts
+++ db/client.ts
@@ -1,6 +1,13 @@
-import { drizzle } from "drizzle-orm/singlestore";
-import pg from "pg";
+import { drizzle } from 'drizzle-orm/node-postgres';
+import pg from 'pg';
 import * as schema from './schema.ts';
+import 'dotenv/config';
 
-const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not set');
+}
+
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 export const db = drizzle(pool, { schema });
--- db/schema.ts
+++ db/schema.ts
@@ -1,11 +1,15 @@
-import { pgTable, serial, bigint, smallint, index } from "drizzle-orm/pg-core";
+import { pgTable, bigint, smallint, index } from 'drizzle-orm/pg-core';
 
-export const bw25 = pgTable('bw25', {
-    id: serial('id').primaryKey(),
-    imgBits: bigint('img_bits', { mode: 'number' }).notNull(),
+export const bw25 = pgTable(
+  'bw25',
+  {
+    imgBits: bigint('img_bits', { mode: 'number' }).primaryKey(),
     tagBits: bigint('tag_bits', { mode: 'number' }).notNull(),
     blackCnt: smallint('black_cnt').notNull(),
-}, (table) => [
+  },
+  (table) => [
+    index('idx_img_bits').on(table.imgBits),
+    index('idx_tag_bits').on(table.tagBits),
     index('idx_black_cnt').on(table.blackCnt),
-    index('idx_tag_bits').on(table.tagBits)
-])
+  ],
+);
