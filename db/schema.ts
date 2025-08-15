7f525b7a1d9315a82a84fd1b7cec60ef31fa3d86
3b7577 <3b7577@gmail.com>
2025-08-15 16:49:50 +0300

    Remove redundant indexes from db schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,15 +1,7 @@
-import { pgTable, bigint, smallint, index } from 'drizzle-orm/pg-core';
+import { pgTable, bigint, smallint } from 'drizzle-orm/pg-core';
 
-export const bw25 = pgTable(
-  'bw25',
-  {
-    imgBits: bigint('img_bits', { mode: 'number' }).primaryKey(),
-    tagBits: bigint('tag_bits', { mode: 'number' }).notNull(),
-    blackCnt: smallint('black_cnt').notNull(),
-  },
-  (table) => [
-    index('idx_img_bits').on(table.imgBits),
-    index('idx_tag_bits').on(table.tagBits),
-    index('idx_black_cnt').on(table.blackCnt),
-  ],
-);
+export const bw25 = pgTable('bw25', {
+  imgBits: bigint('img_bits', { mode: 'number' }).primaryKey(),
+  tagBits: bigint('tag_bits', { mode: 'number' }).notNull(),
+  blackCnt: smallint('black_cnt').notNull(),
+});
